376753e2c51dc5b7090be200477d0d8412f2bcd6
Alvin Seville <alvinseville7cf@gmail.com>
2021-04-15 04:01:19 +0000

    build script updated and added to cd.yml

--- netcorebuild.sh
+++ netcorebuild.sh
@@ -3,7 +3,7 @@
 BUILD_FOLDER_PATH="$1"
 
 shopt -s extglob
-mkdir "$BUILD_FOLDER_PATH"
+mkdir "$BUILD_FOLDER_PATH" || exit
 cd "$BUILD_FOLDER_PATH"
 dotnet new sln -n LaboratoryWork
 dotnet new console -n Project
